2216f47122331eb8d787c47b579e60e4d8468bc0
aupreti1 <anaupreti@gmail.com>
2019-07-08 16:16:27 -0400

    creadted a header and added a little css

--- memorygame/src/components/Header/Header.js
+++ memorygame/src/components/Header/Header.js
@@ -1,2 +1,15 @@
 import React from "react";
 import "./Header.css";
+
+const Header = props => {
+    <header className="header">
+        <h1>
+            Archer's Clicky Game!
+        </h1>
+        <h2>
+            Click On A Pic To Begin
+        </h2>
+    </header>
+};
+
+export default Header;
