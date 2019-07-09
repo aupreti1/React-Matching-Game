70106b30a89947a0ff84745771de9adceace9941
aupreti1 <anaupreti@gmail.com>
2019-07-08 20:45:11 -0400

    added the handle for correct and incoorec click for the items

--- memorygame/src/components/Clicky/Clicky.js
+++ memorygame/src/components/Clicky/Clicky.js
@@ -12,6 +12,24 @@ class Clicky extends Component {
     };
 
     componentDidMount() {
-        this.setState({ item: this.shuffleData(this.forceUpdate.item) });
+        this.setState({ item: this.shuffleItem(this.forceUpdate.item) });
     }
+
+    handleCorrectClick = newItem => {
+        const { topScore, score } = this.state;
+        const newScore = score + 1;
+        const newTopScore = newScore > topScore ? newScore : topScore;
+        this.setState({
+            item: this.shuffleItem(newItem),
+            score: newScore,
+            topScore: newTopScore
+        });
+    };
+
+    handleIncorrectClick = item => {
+        this.setState({
+            item: this.resetItem(item),
+            score: 0
+        });
+    };
 }
